3178d96ae838d6f3cf39561d6aab3e69c1bb4c1a
GET Academy <terje@kolderup.net>
2024-06-18 10:31:14 +0200

    x

--- CountLLines.Test/LindeCountServiceTest.cs
+++ CountLLines.Test/LindeCountServiceTest.cs
@@ -1,5 +1,6 @@
 using CountLines;
 using CountLines.Source;
+using Moq;
 
 namespace CountLLines.Test
 {
@@ -9,8 +10,46 @@ namespace CountLLines.Test
         public void Test1()
         {
             // arrange
-            var source = new MockLineSource();
+            var source = new StringArrayLineSource("Hei", "på", "deg");
             var lineCountService = new LineCountService(source);
+
+            // act
+            var searchText = "e";
+            var stats = lineCountService.GetCounts(searchText);
+
+            // assert
+            Assert.AreEqual(3, stats.TotalLineCount);
+            Assert.AreEqual(2, stats.MatchingLineCount);
+            Assert.AreEqual(searchText, stats.SearchText);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            // arrange
+            var source = new StringArrayLineSource("Hei", "terje", "deg");
+            var lineCountService = new LineCountService(source);
+
+            // act
+            var searchText = "TeRJe";
+            var stats = lineCountService.GetCounts(searchText);
+
+            // assert
+            Assert.AreEqual(3, stats.TotalLineCount);
+            Assert.AreEqual(1, stats.MatchingLineCount);
+            Assert.AreEqual(searchText, stats.SearchText);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            // arrange
+            var mockService = new Mock<ILineSource>();
+            mockService.SetupSequence(lineService => lineService.GetLine())
+                .Returns("Hei")
+                .Returns("på")
+                .Returns("deg");
+            var lineCountService = new LineCountService(mockService.Object);
 
             // act
             var searchText = "e";
---
+++ CountLLines.Test/StringArrayLineSource.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using CountLines.Source;
+
+namespace CountLLines.Test
+{
+    internal class StringArrayLineSource : ILineSource
+    {
+        private IEnumerator _enumerator;
+
+
+        public StringArrayLineSource(params string[] strings)
+        {
+            _enumerator = strings.GetEnumerator();
+        }
+        public string GetLine()
+        {
+            var moveNext = _enumerator.MoveNext();
+            if (!moveNext) return null;
+            return (string)_enumerator.Current;
+        }
+    }
+}
